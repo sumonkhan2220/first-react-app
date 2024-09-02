004e5bd30c5a3ee675b793547fd7f4afadf848af
sumonkhan2220 <saidur1610@gmail.com>
2024-09-02 11:24:22 +0600

    last update

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import AppName from "./components/AppName";
 import AddTodo from "./components/AddTodo";
-import TodoItem1 from "./components/TodoItem1";
 import "./App.css";
 import TodoItem from "./components/TodoItem";
 import WelcomeMessage from "./components/WelcomeMessage";
